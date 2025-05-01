569522dfbc90aff9e480bdbffdc5b4b33e236378
kuekering89 <152684870+kuekering89@users.noreply.github.com>
2025-05-01 19:38:24 +0700

    Update latihan1.php

---
+++ latihan1.php
@@ -0,0 +1,19 @@
+<?php
+
+// $mahasiswa = [
+//     [
+//     "nama" => "Dyra Nadya Putri",
+//     "nrp" => "043040023",
+//     "email" => "diranadia6@gmail.com"
+//     ],
+// ];
+// $mahasiswa = [
+//     "nama" => "caca ",
+//     "nrp" => "043040023",
+//     "email" => "caca7@gmail.com"
+// ];
+
+$data =json_encode($mahasiswa);
+echo $data;
+
+?>
